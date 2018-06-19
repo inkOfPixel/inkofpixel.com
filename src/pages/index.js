a2cdb14cea2f2c6d054564f27feda4b515c2924f
mmarcon91 <m.marcon.91@gmail.com>
2018-06-19 11:38:14 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,15 +83,29 @@ const IndexPage = ({ data }: Props) => {
         <Wrapper>
           <Title>{services.title}</Title>
           <Subtitle>{services.description}</Subtitle>
-          <form name="contact" netlify>
+          <form name="contact" method="POST" netlify>
             <p>
               <label>
-                Name <input type="text" name="name" />
+                Your Name: <input type="text" name="name" />
               </label>
             </p>
             <p>
               <label>
-                Email <input type="email" name="email" />
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Role:{" "}
+                <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message" />
               </label>
             </p>
             <p>
