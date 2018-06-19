2d45baa6e3b67eaed0233a956cc1b8cf118b438d
mmarcon91 <m.marcon.91@gmail.com>
2018-06-19 11:49:57 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -88,7 +88,13 @@ const IndexPage = ({ data }: Props) => {
             method="POST"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
+            action="/form-success"
           >
+            <p class="hidden">
+              <label>
+                Don’t fill this out if you're human: <input name="bot-field" />
+              </label>
+            </p>
             <p>
               <label>
                 Your Name: <input type="text" name="name" />
@@ -97,15 +103,6 @@ const IndexPage = ({ data }: Props) => {
             <p>
               <label>
                 Your Email: <input type="email" name="email" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Role:{" "}
-                <select name="role[]" multiple>
-                  <option value="leader">Leader</option>
-                  <option value="follower">Follower</option>
-                </select>
               </label>
             </p>
             <p>
