91fa139a3d2ceb658215ed2bf60802c2fb8b711e
mmarcon91 <m.marcon.91@gmail.com>
2018-09-10 17:45:34 +0200

    working on contact form #13

--- src/templates/contacts.js
+++ src/templates/contacts.js
@@ -61,37 +61,41 @@ const ContactsPage = ({ data, pathContext }: Props) => {
       </Helmet>
       <Wrapper>
         <Spacer />
-        <PageTitle>{currentPage.title}</PageTitle>
-        <Intro>Drop us a line</Intro>
-
-        <Form
-          name="contact"
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <div className="row hidden">
-            <label>
-              Don’t fill this out if you're human: <input name="bot-field" />
-            </label>
-          </div>
-          <div className="row">
-            <label for="name">Name</label>
-            <input type="text" name="name" />
-          </div>
-          <div className="row">
-            <label for="email">Email</label>
-            <input type="email" name="email" />
-          </div>
-          <div className="row">
-            <label for="message">Message</label>
-            <textarea name="message" />
-          </div>
-          <div className="row submit">
+        <Flexbox>
+          <Info>
+            <PageTitle>{currentPage.title}</PageTitle>
+            <Intro>Let's talk</Intro>
+            <Subtitle>Ask us anything or just say hi...</Subtitle>
+            <Mail>hi@inkofpixel.com</Mail>
+          </Info>
+          <Form
+            name="contact"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <FormField className="hidden">
+              <label>
+                Don’t fill this out if you're human: <input name="bot-field" />
+              </label>
+            </FormField>
+            <FormField className="half">
+              <label for="name">Name</label>
+              <input type="text" name="name" />
+              <span class="focus-border" />
+            </FormField>
+            <FormField className="half">
+              <label for="email">Email</label>
+              <input type="email" name="email" />
+            </FormField>
+            <FormField>
+              <label for="message">Message</label>
+              <textarea name="message" />
+            </FormField>
             <button type="submit">Send</button>
-          </div>
-        </Form>
+          </Form>
+        </Flexbox>
       </Wrapper>
     </Page>
   );
@@ -146,6 +150,18 @@ const Spacer = styled.div`
   width: 100%;
   height: 200px;
 `;
+
+const Flexbox = styled.div`
+  display: flex;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 150px;
+  flex: 0 0 400px;
+  margin-right: 150px;
+`;
 const PageTitle = styled.h1`
   font-size: 14px;
   font-weight: 400;
@@ -154,6 +170,7 @@ const PageTitle = styled.h1`
   position: relative;
   width: 100%;
   color: #05c3b6;
+  font-family: "Roboto Mono", monospace;
   &::before {
     content: "";
     display: block;
@@ -181,9 +198,69 @@ const Intro = styled.h2`
   }
 `;
 
+const Subtitle = styled.p`
+  font-size: 14px;
+  padding: 0;
+  margin: 0;
+  font-weight: 400;
+  line-height: 1.8em;
+  color: #949494;
+  padding-top: 20px;
+`;
+
+const Mail = styled.p`
+  font-size: 14px;
+  padding: 0;
+  margin: 0;
+  font-weight: 400;
+  line-height: 1.8em;
+  color: #949494;
+  padding-top: 60px;
+`;
+
 const Form = styled.form`
-  .hidden {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+const FormField = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: 10px;
+  flex-wrap: wrap;
+  &.half {
+    width: calc(50% - 20px);
+  }
+  &.hidden {
     display: none;
+  }
+  label {
+    font-size: 14px;
+    font-weight: 400;
+    text-transform: uppercase;
+    letter-spacing: 0.1em;
+    position: relative;
+    width: 100%;
+  }
+  input {
+    border: none;
+    position: relative;
+    outline: none;
+    border-bottom: 1px solid #949494;
+  }
+  input ~ .focus-border {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 0;
+    height: 1px;
+    background-color: #161338;
+    transition: 0.4s;
+  }
+  input:focus ~ .focus-border {
+    width: 100%;
+    transition: 0.4s;
   }
 `;
 
